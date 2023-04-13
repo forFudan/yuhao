f7aea1f6a4befdb848cf20436d8f993ce9b7cddd
forFudan <dr.yuhao.zhu@outlook.com>
2023-04-13 13:17:58 +0200

    trial new search engine

--- chaifen/app.js
+++ chaifen/app.js
@@ -1,4 +1,4 @@
-d3.csv("宇浩輸入法全漢字拆分表.csv", function(data) {
+d3.csv("宇浩輸入法全漢字拆分表.csv", function (data) {
     var dictionary = data;
     var button = d3.select("#button");
     var form = d3.select("#form");
@@ -10,18 +10,21 @@ d3.csv("宇浩輸入法全漢字拆分表.csv", function(data) {
         d3.select("tbody").html("")
         d3.event.preventDefault();
         var inputValue = d3.select("#user-input").property("value");
-        var filteredDictionary =
-            dictionary.filter(dictionary => dictionary.char.includes(inputValue));
-        var output = _.sortBy(filteredDictionary, 'code')
-        for (var i = 0; i < filteredDictionary.length; i++) {
-            d3.select("tbody").insert("tr").html(
-                "<td>" + (output[i]['char']) + "</td>" +
-                "<td>" + (output[i]['division']) + "</td>" +
-                "<td>" + (output[i]['code']) + "</td>" +
-                "<td>" + (output[i]['quick_simp']) + "</td>" +
-                "<td>" + (output[i]['region']) + "</td>" +
-                "<td>" + (output[i]['details']) + "</td>" +
-                "<td>" + (output[i]['comments']) + "</td>")
+        var inputChars = inputValue.split('');
+        for (var n = 0; n < inputChars.length; n++) {
+            var filteredDictionary =
+                dictionary.filter(dictionary => dictionary.char.includes(inputChars[n]));
+            var output = _.sortBy(filteredDictionary, 'code')
+            for (var i = 0; i < filteredDictionary.length; i++) {
+                d3.select("tbody").insert("tr").html(
+                    "<td>" + (output[i]['char']) + "</td>" +
+                    "<td>" + (output[i]['division']) + "</td>" +
+                    "<td>" + (output[i]['code']) + "</td>" +
+                    "<td>" + (output[i]['quick_simp']) + "</td>" +
+                    "<td>" + (output[i]['region']) + "</td>" +
+                    "<td>" + (output[i]['details']) + "</td>" +
+                    "<td>" + (output[i]['comments']) + "</td>")
+            }
         }
     };
 });
