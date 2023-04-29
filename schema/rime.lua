4ddbd070e20f9f018ef7ff12a041e65bb8c32e13
Yuhao Zhu, PhD, FRM <dr.yuhao.zhu@outlook.com>
2023-04-30 01:33:15 +0200

    全面更新拆分查詢系統

--- schema/rime.lua
+++ schema/rime.lua
@@ -7,3 +7,6 @@ yuhao_char_only = yuhao_char_filter.yuhao_char_only
 yuhao_single_char_only_for_full_code = require("yuhao/yuhao_single_char_only_for_full_code")
 yuhao_postpone_full_code = require("yuhao/yuhao_postpone_full_code")
 yuhao_helper = require("yuhao/yuhao_helper")
+temp = require("yuhao/yuhao_chaifen")
+yuhao_chaifen = temp.filter
+yuhao_chaifen_processor = temp.processor
