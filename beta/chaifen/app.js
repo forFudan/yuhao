482a686afa925f04903cbd61867abfe96a70ce5a
Yuhao Zhu, PhD, FRM <dr.yuhao.zhu@outlook.com>
2023-05-19 23:54:22 +0200

    增加臺灣字形支持。

--- beta/chaifen/app.js
+++ beta/chaifen/app.js
@@ -20,7 +20,7 @@ d3.csv("https://zhuyuhao.com/yuhao/beta/chaifen/宇浩輸入法全漢字拆分�
                     "<td>" + (output[i]['char']) + "</td>" +
                     "<td>" + (output[i]['division']) + "</td>" +
                     "<td>" + (output[i]['code']) + "</td>" +
-                    "<td>" + (output[i]['quick_simp']) + "</td>" +
+                    // "<td>" + (output[i]['quick_simp']) + "</td>" +
                     "<td>" + (output[i]['division_tw']) + "</td>" +
                     "<td>" + (output[i]['region']) + "</td>" +
                     // "<td>" + (output[i]['details']) + "</td>" +
