84619c468db7f5ac2cdd9fe40f8a733fb1637298
Yuhao Zhu, PhD, FRM <dr.yuhao.zhu@outlook.com>
2023-11-23 18:43:22 +0100

    Update the UI of chaifen for yustar

--- star/chaifen/app.js
+++ star/chaifen/app.js
@@ -2,12 +2,11 @@ d3.csv("https://zhuyuhao.com/yuhao/star/chaifen/宇浩星陳全漢字拆分表.c
     var dictionary = data;
     var button = d3.select("#button");
     var form = d3.select("#form");
-    button.on("click", runEnter);
-    form.on("submit", runEnter);
+    form.on("keyup", runEnter);
 
     // Defining the function
     function runEnter() {
-        d3.select("tbody").html("")
+        d3.select("#tbody").html("")
         d3.event.preventDefault();
         var inputValue = d3.select("#user-input").property("value");
         var inputChars = [...inputValue];
@@ -16,25 +15,45 @@ d3.csv("https://zhuyuhao.com/yuhao/star/chaifen/宇浩星陳全漢字拆分表.c
                 dictionary.filter(dictionary => dictionary.char.includes(inputChars[n]));
             var output = _.sortBy(filteredDictionary, 'code')
             for (var i = 0; i < filteredDictionary.length; i++) {
-                d3.select("tbody").insert("tr").html(
+                d3.select("#tbody").insert("tr").html(
                     "<td><a href='https://zi.tools/zi/" + (output[i]['char']) + "'>" + (output[i]['char']) + "</a></td>" 
                     +
                     "<td>" + (output[i]['division']) + "</td>" 
                     +
                     "<td>" + (output[i]['code']) + "</td>" 
                     +
-                    // "<td>" + (output[i]['quick_simp']) + "</td>" +
                     "<td>" + (output[i]['division_tw']) + "</td>" 
                     +
                     "<td>" + (output[i]['code_tw']) + "</td>" 
                     +
-                    "<td>" + (output[i]['region']) + "</td>" 
-                    // +
-                    // "<td>" + (output[i]['details']) + "</td>" 
-                    // +
-                    // "<td>" + (output[i]['comments']) + "</td>"
+                    "<td>" + (output[i]['region']) + "</td>"
                     )
             }
         }
+
+        // d3.select("#tbody2").html("")
+        // d3.event.preventDefault();
+        // var inputValue = d3.select("#user-input").property("value");
+        // var inputChars = [...inputValue];
+        // for (var n = 0; n < inputChars.length; n++) {
+        //     var filteredDictionary =
+        //         dictionary.filter(dictionary => dictionary.char.includes(inputChars[n]));
+        //     var output = _.sortBy(filteredDictionary, 'code')
+        //     for (var i = 0; i < filteredDictionary.length; i++) {
+        //         d3.select("#tbody2").insert("tr").html(
+        //             "<td><a href='https://zi.tools/zi/" + (output[i]['char']) + "'>" + (output[i]['char']) + "</a></td>" 
+        //             +
+        //             "<td>" + (output[i]['division']) + "</td>" 
+        //             +
+        //             "<td>" + (output[i]['code']) + "</td>" 
+        //             +
+        //             "<td>" + (output[i]['division_tw']) + "</td>" 
+        //             +
+        //             "<td>" + (output[i]['code_tw']) + "</td>" 
+        //             +
+        //             "<td>" + (output[i]['region']) + "</td>"
+        //             )
+        //     }
+        // }
     };
 });
