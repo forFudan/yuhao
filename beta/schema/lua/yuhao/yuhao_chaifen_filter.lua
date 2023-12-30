b3d68124311fc5a1da8a3ccbd453cca98e11163c
forFudan <dr.yuhao.zhu@outlook.com>
2023-12-30 19:01:54 +0100

    Remove "beta" folder

--- beta/schema/lua/yuhao/yuhao_chaifen_filter.lua
+++
@@ -1,2 +0,0 @@
-local yuhao_chaifen_filter = require("yuhao.yuhao_chaifen").filter
-return yuhao_chaifen_filter
--- beta/schema/lua/yuhao/yuhao_switch_proc.lua
+++
@@ -1,2 +0,0 @@
-local yuhao_switch_proc = require("yuhao.yuhao_switch").proc
-return yuhao_switch_proc
